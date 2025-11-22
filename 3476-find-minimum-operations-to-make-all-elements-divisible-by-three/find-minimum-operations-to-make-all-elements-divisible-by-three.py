dfa46b990b3841a8897910ad62ab0c8c313bc32f
Kaithepalli Harish Kumar <163495794+HARISHkaithepalli@users.noreply.github.com>
2025-11-22 20:22:38 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (98.19%) - LeetSync

---
+++ 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def minimumOperations(self, nums):
+        operations = 0
+        for x in nums:
+            if x % 3 != 0:
+                operations += 1
+        return operations
+        
